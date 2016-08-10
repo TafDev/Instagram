a9ed7e01338f9a2ea32caf912cc00418fd017acd
TafDev <chombot@hotmail.com>
2016-08-10 21:43:48 +0100

    link user and post table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :user_name, presence: true, length: {minimum: 4, maximum: 16}
